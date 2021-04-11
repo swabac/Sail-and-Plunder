031a0c4ff9e306d7c5f0e3ec6a6045b0c92f74fd
Djordje Jovišić <djordje.jovisic@gmail.com>
2021-04-11 20:57:49 +0200

    Character movement (forward and sideways)

---
+++ Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private SwerveInputSystem _swerveInputSystem;
+    [SerializeField] private float swerveSpeed = 0.5f;
+    [SerializeField] private float maxSwerveAmount = 1f;
+    [SerializeField] private float forwardSpeed = 1f;
+    
+    private void Awake()
+    {
+        _swerveInputSystem = GetComponent<SwerveInputSystem>();
+    }
+
+    private void Update()
+    {
+        float swerveAmount = Time.deltaTime * swerveSpeed * _swerveInputSystem.MoveFactorX;
+        float forwardAmount = Time.deltaTime * forwardSpeed;
+        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
+        transform.Translate(swerveAmount, -forwardAmount, 0);
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   // private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    void Start()
+    {
+       // controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForward = Time.deltaTime * forwardSpeed;
+        
+        transform.Translate(0, -moveForward,0 );
+    }
+    private void FixedUpdate()
+    {
+       // controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
